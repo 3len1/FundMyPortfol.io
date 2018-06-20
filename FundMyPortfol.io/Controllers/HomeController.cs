8fefb871e40aacca18da3992b9634cb0bc04336c
Eleni Vasilaki <evasilaki@cognity.gr>
2018-06-20 21:00:05 +0300

    ^.^ love ya

--- FundMyPortfol.io/Controllers/HomeController.cs
+++ FundMyPortfol.io/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace FundMyPortfol.io.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "About FundMyPortfol.io.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Find us.";
 
             return View();
         }
