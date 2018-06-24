73c7e88f4f534debbf5c835e5e8b3f5755430221
Maria Petrogiannou <mar-petr2010@hotmail.com>
2018-06-24 13:41:40 +0300

    ^.^

--- Database/V_2__ChangeColumns.sql
+++ Database/V_2__ChangeColumns.sql
@@ -4,4 +4,4 @@ alter table dbo.Package drop column DeliveryDate;
 
 alter table dbo.BackerBuyPackage add DeliveryDate datetime;
 
-alter table dbo.Project add Category nvarchar(100) not null default('innovation');
+alter table dbo.Project add Category nvarchar(100) not null default('INNOVATION');
